2630e63ee79a5cd4aac421cf105e77c001639695
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2017-12-12 18:34:32 +0000

    level 2 re-made, timer not completely working

--- js/script.js
+++ js/script.js
@@ -14,26 +14,26 @@ function setup() {
 
   const $instructionPage = $('.instructionPage');
   const $levelOnePage = $('.levelOnePage');
+  const $levelTwoPage = $('.levelTwoPage');
   const $tryAgainPage = $('.tryAgainPage');
   const $playerPassedLevelOnePage = $('.playerPassedLevelOne');
-  // const $playerPassedLevelTwoPage = $('.playerPassedLevelTwo');
+  const $playerPassedLevelTwoPage = $('.playerPassedLevelTwo');
   const $beginButton = $('.beginButton');
   const $displayTimerOnScreen = $('.timerDisplay');
   const $startAgainButton = $('.startAgainButton');
   const $displayColor = $('.colorWordWithDifferentColor');
   const $colorButtons = $('.colorsForPlayerToPickFrom li');
-  const $rounds = $('.gameRounds li');
+  let $rounds = $('.gameRounds li');
   const $finalScoreTime = $('.finalScoreTime');
   const $playLevelTwoButton = $('.playLevelTwoButton');
-  // const $playLevelThreeButton = $('.playerLevelThreeButton');
-  const $backToInstructionPageFromTryAgainPageButton = $('.backToInstructionPageFromTryAgainPage');
-  const $backToInstructionPageFromPlayerWonPageButton = $('.backToInstructionPageFromPlayerWonPage');
+  const $homepageFromTryAgainPageButton = $('.homepageFromTryAgainPage');
+  const $homepagePageFromPassedLevelOneButton = $('.homepageFromPassedLevelOne');
+  const $homepagePageFromPassedLevelTwoButton = $('.homepageFromPassedLevelTwo');
 
 
   function beginLevelOne(){
     $instructionPage.hide();
     $levelOnePage.show();
-    level = 1;
     startTimer();
     colorRandomlySelected();
   }
@@ -50,6 +50,7 @@ function setup() {
       if(timeRemaining === 0) {
         clearInterval(timerId);
         $levelOnePage.hide();
+        $levelTwoPage.hide();
         $tryAgainPage.show();
         timeGiven = 8;
       }
@@ -61,6 +62,7 @@ function setup() {
     round = 0;
     $rounds.css('backgroundColor', 'rgba(255, 255, 255, 0.41)');
     $tryAgainPage.hide();
+    $playerPassedLevelOnePage.hide();
     $levelOnePage.show();
     resetTimer();
     colorRandomlySelected();
@@ -80,33 +82,32 @@ function setup() {
     const chosenWord = generateRandomColor();
     colorOfText = generateRandomColor();
     $displayColor.text(chosenWord).css('color', colorOfText);
-    if (level <= 1){
-      $displayColor;
-    } else if (level <= 2){
-      $displayColor.addClass('level-2-animation');
-    // } else {
-    //   $displayColor.addClass('level-2-animation');
-    //   $colorButtons.css('backgroundColor', 'red');
-    }
   }
 
 
   function completedRound(){
-    $rounds.eq(round).css('backgroundColor', 'black');
+    $rounds.eq(round).css('backgroundColor', 'rgba(0,0,0,1)');
+    console.log($rounds.eq(round).css('backgroundColor'));
     round++;
     if (round >= 8){
+      console.log('in here','level:',level);
       clearInterval(timerId);
       secondsTaken = (((new Date().getTime() - startTime)/1000).toFixed(1));
       $finalScoreTime.html(`${'You finished the level in:'}` + ' ' + secondsTaken + ' ' + 'secs');
+      // console.log('timer stopped because end of round');
+      if(level <= 1){
+        $levelOnePage.hide();
+        $playerPassedLevelOnePage.show();
+      }else {
+        $levelTwoPage.hide();
+        $playerPassedLevelTwoPage.show();
+      }
       level++;
-      console.log('timer stopped because end of round');
-      $levelOnePage.hide();
-      $playerPassedLevelOnePage.show();
     } else {
       timeGiven--;
       secondsTaken = 0;
       resetTimer();
-      console.log('start timer again because not end of round yet');
+      // console.log('start timer again because not end of round yet');
       colorRandomlySelected();
     }
   }
@@ -117,46 +118,55 @@ function setup() {
       completedRound();
     }else if(colorOfText !== $(e.target).text()){
       $levelOnePage.hide();
+      $levelTwoPage.hide();
       $tryAgainPage.show();
       timeGiven = 8;
     }
   }
 
-
   function beginLevelTwo (){
     timeGiven = 8;
     round = 0;
+    $rounds = $('.gameRounds li.levelTwo');
+    console.log($rounds);
     $rounds.css('backgroundColor', 'rgba(255, 255, 255, 0.41)');
     $playerPassedLevelOnePage.hide();
-    $levelOnePage.show();
+    $levelTwoPage.show();
     resetTimer();
     colorRandomlySelected();
+
   }
 
-  // function beginLevelThree(){
-  //   timeGiven = 8;
-  //   round = 0;
-  //   $rounds.css('backgroundColor', 'rgba(255, 255, 255, 0.41)');
-  //   $playerPassedLevelTwoPage.hide();
-  //   $levelOnePage.show();
-  //   resetTimer();
-  //   colorRandomlySelected();
-  // }
 
-  function backToInstructionPageFromPlayerWonPage() {
+  // HOMEPAGE
+  function homepageFromPassedLevelOne() {
     timeGiven = 8;
     round = 0;
+    $rounds = $('.gameRounds li')
     $rounds.css('backgroundColor', 'rgba(255, 255, 255, 0.41)');
     $playerPassedLevelOnePage.hide();
     $instructionPage.show();
+    resetTimer();
   }
 
-  function backToInstructionPageFromTryAgainPage() {
+  function homepageFromPassedLevelTwo() {
     timeGiven = 8;
     round = 0;
+    $rounds = $('.gameRounds li')
+    $rounds.css('backgroundColor', 'rgba(255, 255, 255, 0.41)');
+    $playerPassedLevelTwoPage.hide();
+    $instructionPage.show();
+    resetTimer();
+  }
+
+  function homepageFromTryAgainPage() {
+    timeGiven = 8;
+    round = 0;
+    $rounds = $('.gameRounds li')
     $rounds.css('backgroundColor', 'rgba(255, 255, 255, 0.41)');
     $tryAgainPage.hide();
     $instructionPage.show();
+    resetTimer();
   }
 
 
@@ -168,19 +178,20 @@ function setup() {
 
   $startAgainButton.on('click', startAgain);
 
-  $backToInstructionPageFromTryAgainPageButton.on('click', backToInstructionPageFromTryAgainPage);
+  $homepageFromTryAgainPageButton.on('click', homepageFromTryAgainPage);
 
   $playLevelTwoButton.on('click', beginLevelTwo);
 
-  // $playLevelThreeButton.on('click', beginLevelThree);
+  $homepagePageFromPassedLevelOneButton.on('click', homepageFromPassedLevelOne);
 
-  $backToInstructionPageFromPlayerWonPageButton.on('click', backToInstructionPageFromPlayerWonPage);
+  $homepagePageFromPassedLevelTwoButton.on('click', homepageFromPassedLevelTwo);
 
 
 }
 
 $(setup);
 
+//TOMORROW - ADD LEVEL 3 AND RANDOMISE COLORS AND REFACTOR CODE
 
 // TO RANDOMLY CHANGE THE CONTENT OF COLOR BUTTONS for LEVEL 3:
 //
