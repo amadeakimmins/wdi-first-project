cc92d7c76cb7d45a445b74817dff5309777d1949
Amadea Kimmins <amadea.kimmins@hotmail.co.uk>
2017-12-14 13:22:56 +0000

    out of test mode - 8 rounds

--- js/script.js
+++ js/script.js
@@ -170,7 +170,7 @@ function setup() {
   function completedRound(){
     $rounds.eq(round).css('backgroundColor', 'rgba(0,0,0,1)');
     round++;
-    if (round >= 1){
+    if (round >= 8){
       clearInterval(timerId);
       secondsTaken = (((new Date().getTime() - startTime)/1000).toFixed(1));
       $finalScoreTime.html(`${'You finished the level in:'}` + ' ' + secondsTaken + ' ' + 'secs');
